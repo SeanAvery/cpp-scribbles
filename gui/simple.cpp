9ccd8e9d37ea9a96f30a3bb817907a7f1f575a31
SeanAvery <seanpollock3344@gmail.com>
2018-04-17 12:22:48 -0400

    small fixes. window working

--- gui/simple.cpp
+++ gui/simple.cpp
@@ -1,14 +1,14 @@
 #include <QApplication>
 #include <QWidget>
 
-int main(int argc, char** argv[])
+int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
 	QWidget window;
 	
 	window.resize(250, 150);
-	window.setWindow("look at dis");
+	window.setWindowTitle("look at dis");
 	window.show();
 
 	return app.exec();
