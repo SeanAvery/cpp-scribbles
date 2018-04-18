c4c1cd77b4043c41eb761787c21f213c1bcbecce
Sean Pollock <seanpollock3344@gmail.com>
2018-04-18 16:49:04 -0400

    succesfully reading video file

--- video_processing/Processor.cpp
+++ video_processing/Processor.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 #include <string>
+#include <iomanip>  // for controlling float print precision
+#include <sstream>
+
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace std;
 using namespace cv;
 
-int main(int argc, char** argv[])
+int main(int argc, char *argv[])
 {
 		if (argc != 2)
 		{
-			  cout << "Not enough parameters" << end1;
+			  cout << "Not enough parameters" << endl;
 			  return -1;
 		}
 
 		const string videoFileName = argv[1];
 
 		VideoCapture captRefrnc(videoFileName);
-		captUndTst.open(videoFileName);
+		VideoCapture captUndTst(videoFileName);
 
 		if (!captRefrnc.isOpened())
 		{
-				cout << "Could not open movie file" << videoFileName << end1;
+				cout << "Could not open movie file" << videoFileName << endl;
 				return -1;
 		}
 }
