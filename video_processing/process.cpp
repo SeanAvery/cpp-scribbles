bf8a22e24ab6862f9a56afef114af024ff918060
Sean Pollock <seanpollock3344@gmail.com>
2018-04-18 14:27:32 -0400

    adding executable def and opencv dependency

---
+++ video_processing/process.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Not enough parameters" << end1;
+		return -1;
+	}
+}
